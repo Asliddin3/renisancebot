ddee8f4328b7a3508e3cc2310c2503ff0f3e646d
Asliddin3 <asliddindeh@mail.ru>
2023-07-18 22:23:11 +0500

    added filters

--- ebozor/products/admin.py
+++ ebozor/products/admin.py
@@ -8,10 +8,10 @@ from .models import User,Contract,Fakultet,Test
 admin.site.register(User)
 admin.site.register(Test)
 admin.site.register(Fakultet)
-admin.site.register(Contract)
+# admin.site.register(Contract)
  # Replace 'your_column_name' with the actual column name
 
-# @admin.register(Contract)
-# class ContractAdmin(admin.ModelAdmin):
-#     list_display = Contract.DisplayFields
-#     search_fields = Contract.SearchableFields
+@admin.register(Contract)
+class ContractAdmin(admin.ModelAdmin):
+    list_display = Contract.DisplayFields
+    search_fields = Contract.SearchableFields
