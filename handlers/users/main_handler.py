2ce3c56873aa45b48447bc7e7784d562529ee470
Asliddin3 <asliddindeh@mail.ru>
2023-07-20 21:30:21 +0500

    added language

--- handlers/users/main_handler.py
+++ handlers/users/main_handler.py
@@ -477,7 +477,7 @@ async def accept_student(message:Message,contract_id:int,created:datetime):
             "Ta’lim shakli:": Times[full_info[5]],
             "O‘qish muddati:":f"{year}-yil({finishYear})",
             "O‘quv kursi:": "1-bosqich",
-            "Ta’lim yo‘nalishi:": full_info[4]
+            "Ta’lim yo‘nalishi:":  f"{full_info[4]}\n{langDic[full_info[8]]}"
         }
     }
     create_contract(data)
