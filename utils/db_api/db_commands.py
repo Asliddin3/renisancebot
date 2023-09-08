aa57be7c96ff2953b6f825d71f5840a5882ba05f
Asliddin3 <asliddindeh@mail.ru>
2023-09-08 13:39:12 +0500

    added new

--- utils/db_api/db_commands.py
+++ utils/db_api/db_commands.py
@@ -141,7 +141,7 @@ class Database:
     async def get_contract_by_id(self,id):
         sql="SELECT products_contract.id,full_name,phone,extra_phone,f.name,f.time,f.lang,address,passport,jshshir,passport_photo," \
             "dtm,result,created,result,diplom " \
-            " FROM products_contract INNER JOIN products_fakultet AS f " \
+            " FROM products_contract LEFT JOIN products_fakultet AS f " \
             "ON f.id=fakultet_id  WHERE state !='new' AND products_contract.id=$1"
         return await self.execute(sql,id,fetch=True)
 
