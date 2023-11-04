d31cbb9eaf8245605fdbf5f298157bce9b9b90fa
Asliddin3 <asliddindeh@mail.ru>
2023-11-04 21:55:14 +0500

    fxi

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -2,12 +2,13 @@ import logging
 
 from aiogram import Dispatcher
 
-from data.config import ADMINS
+from data.config import DEVELOPER
 
 
 async def on_startup_notify(dp: Dispatcher):
-    for admin in ADMINS:
+    for admin in DEVELOPER:
         try:
+
             await dp.bot.send_message(admin, "Bot ishga tushdi")
 
         except Exception as err:
